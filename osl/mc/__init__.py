89e901ed640e7b2a24d9bdbea8e506944fbc2544
Michael Mark <92mark.michael@gmail.com>
2020-06-02 15:26:11 +0200

    macbook branch sync

---
+++ osl/mc/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__version__ = "0.0.1"
+__author__ = "Michael Mark"
+
+# Monte Carlo Optimal Sustain Level
+
---
+++ osl/mc/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
+import numpy
+import os
+
+
+def find_pyx(path='.'):
+    pyx_files = []
+    for root, dirs, filenames in os.walk(path):
+        for fname in filenames:
+            if fname.endswith('.pyx'):
+                pyx_files.append(os.path.join(root, fname))
+    return pyx_files
+
+
+extensions = [
+    Extension("sustained_ihp_c", ["sustained_ihp_c.pyx"], include_dirs=[numpy.get_include()])
+]
+os.environ["CC"] = "g++-9"
+os.environ["CXX"] = "g++-9"
+setup(
+    name='sustained_ihp_c',
+    ext_modules=cythonize(extensions),
+    script_args=["build_ext", "--inplace"]
+)
