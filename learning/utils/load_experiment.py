13a230bc50adf1e058b5127d948fbe4182cde2ca
markm <michael.mark@epfl.ch>
2021-05-06 10:33:40 +0200

    loading full experiments added

--- learning/utils/load_experiment.py
+++ learning/utils/load_experiment.py
@@ -4,16 +4,50 @@ from learning.utils import misc
 
 
 class LoadExperiment:
-    def __init__(self, backup=False):
+    def __init__(self, experiment=None):
         """
 
         Args:
             backup:
         """
-        if backup:
-            self.repo = os.path.join(misc.RESOURCE_ROOT, 'backup')
+        self.repo = os.path.join(misc.RESOURCE_ROOT)
+        self.experiment = experiment
+
+    def list_names_in_experiment(self, return_paths=True):
+        assert self.experiment is not None, 'No experiment name specified'
+        path = os.path.join(self.repo, 'models', self.experiment)
+        dirs = os.listdir(path)
+        if return_paths:
+            return_res = []
+            for d in dirs:
+                full_path = os.path.join(path, d)
+                return_res.append(full_path)
         else:
-            self.repo = os.path.join(misc.RESOURCE_ROOT)
+            return_res = dirs
+        return return_res
+
+    def list_experiment_checkpoints(self, log_number):
+        """
+
+        Args:
+            name:
+            log_num:
+            returntype:
+
+        Returns:
+
+        """
+        names_in_experiment = self.list_names_in_experiment(return_paths=True)
+        all_checkpoints = []
+        for name in names_in_experiment:
+            logs = sorted(os.listdir(name))
+            path_to_checkpoints = os.path.join(name, logs[log_number], 'checkpoints')
+            assert os.path.exists(path_to_checkpoints)
+
+            checkpoints = sorted([int(c) for c in os.listdir(path_to_checkpoints)])
+            all_checkpoints = all_checkpoints + [os.path.join(path_to_checkpoints, str(checkpoint)) for checkpoint in checkpoints]
+
+        return all_checkpoints
 
     def list_logs(self, name, return_type='paths'):
         """
@@ -85,9 +119,14 @@ class LoadExperiment:
 
 
 if __name__ == '__main__':
-    ldr = LoadExperiment(backup=True)
-    print(ldr.list_logs('DQN-0'))
-    print(ldr.list_checkpoints('DQN-0', 0))
+    ldr = LoadExperiment('honiklada')
+    print(ldr.list_names_in_experiment())
+    cps = ldr.list_experiment_checkpoints(log_number=0)
+    print(cps)
+    ag = ldr.load_agent_from_path(cps[1])
 
-    cps = ldr.list_checkpoints('MONOSPLINE-0', 0)
-    ag = ldr.load_agent_from_path(cps[3])
+    # print(ldr.list_logs('DQN-0'))
+    # print(ldr.list_checkpoints('DQN-0', 0))
+    #
+    # cps = ldr.list_checkpoints('MONOSPLINE-0', 0)
+    # ag = ldr.load_agent_from_path(cps[3])
